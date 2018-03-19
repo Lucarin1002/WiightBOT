181b54b063d5a44f9e3ef20a124ef2f24e84ce83
wiigevaerd <30363144+wiigevaerd@users.noreply.github.com>
2018-03-18 23:11:38 -0300

    WiightApp

--- app.js
+++ app.js
@@ -51,11 +51,13 @@ client.on('message' , message => {
     .setThumbnail(client.user.avatarURL)
     .addField('Ajuda:', 'Veja os meus comandos ^-^')
     .addField('w!coinflip', 'É um simples cara ou coroa.')
-    .addField("w!gerarsenha", "Gera um número aleatório que pode servir como uma senha temporária para vc.")
     .addField("w!ping", "Utilize esse comando para ver o meu tempo de resposta.")
-    .addField("w!cc", "Utilize para limpar o chat atual. (Necessita estar com a tag `wStaff` no servidor desejado")
-    .addField("Atenção:", "Há comandos como o w!cc que necessitam da tag `wStaff` para ser utilizado o comando.")
-    .setFooter('WiightBOT by wiigevaerd#0202 ')
+    .addField("w!gerarsenha", "Gera um número aleatório que pode servir como uma senha temporária para vc.")
+    .addField('────────────── Comandos De Moderacão ──────────────', 'Os comandos de moderação necessitam da tag `wStaff` para funcionarem')
+    .addField("w!cc", "Comando basico para limpar o chat.")
+    .addField("w!mute", "Comando de mute, necessita da tag `Mutado` para funcionar.")
+    .addField("w!unmute", "Comando de unmute, necessita da tag `Mutado` para funcionar.")
+    .setFooter('WiightBOT by wiigevaerd#0866 ')
     message.author.send(helpembed);
     message.delete();
     message.reply(" Enviei para você no privado, veja suas mensagens diretas! :wink:");
