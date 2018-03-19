bcd801dc5c45b4b24235a9c0f79a10c370972239
wiigevaerd <30363144+wiigevaerd@users.noreply.github.com>
2018-03-18 23:31:00 -0300

    WiightInvite

--- app.js
+++ app.js
@@ -63,3 +63,18 @@ client.on('message' , message => {
     message.reply(" Enviei para você no privado, veja suas mensagens diretas! :wink:");
   }
 });
+
+client.on('message' , message => {
+  if(message.content.startsWith(prefix + 'invite')) {
+    const helpembed = new Discord.RichEmbed()
+    .setColor('#ff00b2')
+    .setTimestamp()
+    .setThumbnail(client.user.avatarURL)
+    .addField('Invite', 'você pode me adcionar no seu servidor com o link abaixo ^-^')
+    .addField('https://discordapp.com/oauth2/authorize?client_id=416645125981208579&permissions=8&scope=bot')
+    .setFooter('WiightBOT by wiigevaerd#0866 ')
+    message.author.send(helpembed);
+    message.delete();
+    message.reply("veja suas mensagens privadas! :wink:");
+  }
+});
