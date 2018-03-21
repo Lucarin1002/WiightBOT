7a7d0c06ba9f0e9fd2567ef45b928b82d7cb3867
wiigevaerd <30363144+wiigevaerd@users.noreply.github.com>
2018-03-21 00:59:56 -0300

    WiightUpdate

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ if(message.content.startsWith('w!invite')){
   const helpembed = new Discord.RichEmbed()
   .setColor('#ff00b2')
   .setTimestamp()
-  .setThumbnail(client.user.avatarURL)
+  .setThumbnail('https://i.imgur.com/HkTaKoa.png')
   .addField('Invite', 'você pode me adcionar no seu servidor com o link abaixo ^-^')
   .addField('────── Link ────────', 'https://discordapp.com/oauth2/authorize?client_id=416645125981208579&permissions=8&scope=bot')
   .setFooter('WiightBOT by wiigevaerd#0866 ')
