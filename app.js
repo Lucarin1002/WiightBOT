dae10c9f29b41626548faf4b261d3eb5d3adb8b9
wiigevaerd <30363144+wiigevaerd@users.noreply.github.com>
2018-03-21 00:50:18 -0300

    WiightUpdate

--- app.js
+++ app.js
@@ -40,3 +40,47 @@ if(!message.content.startsWith(prefix)) return;
     console.error(err);
   }
 });
+
+client.on('message', message => {
+
+  if(message.content.startsWith('w!ajuda')){
+
+  const helpembed = new Discord.RichEmbed()
+  .setColor('#00ffed')
+  .setTimestamp()
+  .setThumbnail(client.user.avatarURL)
+  .addField('Ajuda:', 'Veja os meus comandos ^-^')
+  .addField('w!coinflip', 'É um simples cara ou coroa.')
+  .addField("w!ping", "Utilize esse comando para ver o meu tempo de resposta.")
+  .addField("w!gerarsenha", "Gera um número aleatório que pode servir como uma senha temporária para vc.")
+  .addField('────── Comandos De Moderacão ────────', 'Os comandos de moderação necessitam da tag `wStaff` para funcionarem')
+  .addField("w!cc", "Comando basico para limpar o chat.")
+  .addField("w!mute", "Comando de mute, necessita da tag `Mutado` para funcionar.")
+  .addField("w!unmute", "Comando de unmute, necessita da tag `Mutado` para funcionar.")
+  .setFooter('WiightBOT by wiigevaerd#0866 ')
+  message.author.send(helpembed);
+  message.delete();
+  message.reply(" Enviei para você no privado, veja suas mensagens diretas! :wink:");
+
+}
+
+});
+
+client.on('message', message => {
+
+if(message.content.startsWith('w!invite')){
+
+  const helpembed = new Discord.RichEmbed()
+  .setColor('#ff00b2')
+  .setTimestamp()
+  .setThumbnail(client.user.avatarURL)
+  .addField('Invite', 'você pode me adcionar no seu servidor com o link abaixo ^-^')
+  .addField('Link:', 'https://discordapp.com/oauth2/authorize?client_id=416645125981208579&permissions=8&scope=bot')
+  .setFooter('WiightBOT by wiigevaerd#0866 ')
+  message.author.send(helpembed);
+  message.delete();
+  message.reply("veja suas mensagens privadas! :wink:");
+  
+}
+
+});
